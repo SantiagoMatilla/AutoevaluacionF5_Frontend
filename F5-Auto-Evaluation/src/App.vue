44843029c211a4234c6c6d43d596912004fd4ed7
victorteje <vteje2@gmail.com>
2023-09-22 14:14:46 +0200

    created knowledge squares

--- F5-Auto-Evaluation/src/App.vue
+++ F5-Auto-Evaluation/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Knowledge from './components/Knowledge.vue';
 </script>
 
 <template>
+  <Knowledge />
   <RouterView />
 </template>
 
---
+++ F5-Auto-Evaluation/src/components/Knowledge.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <main>
+        <section id="knowledge">
+            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Investigar</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Recordar</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Comprender</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Aplicar</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Analizar</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Crear</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Evaluar</v-sheet>
+        </section>
+    </main>
+</template>
+
+
+<style scoped>
+
+#knowledge {
+    margin: auto;
+    width: 70%;
+    display: flex;
+    justify-content: space-around;
+}
+
+.knowledgeSquare {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: small;
+}
+
+</style>
