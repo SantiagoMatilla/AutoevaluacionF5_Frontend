e713a40cb7118d2e937ed8bc5635655bcd24c082
victorteje <vteje2@gmail.com>
2023-09-22 14:45:03 +0200

    created knowledge squares

--- F5-Auto-Evaluation/src/components/Knowledge.vue
+++ F5-Auto-Evaluation/src/components/Knowledge.vue
@@ -6,13 +6,13 @@
 <template>
     <main>
         <section id="knowledge">
-            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Investigar</v-sheet>
-            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Recordar</v-sheet>
-            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Comprender</v-sheet>
-            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Aplicar</v-sheet>
-            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Analizar</v-sheet>
-            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Crear</v-sheet>
-            <v-sheet class="knowledgeSquare" :height="80" :width="80" color="blue-grey-darken-1" rounded :elevation="24">Evaluar</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="100" :width="100" color="blue-grey-darken-1" rounded :elevation="24">Investigar</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="100" :width="100" color="blue-grey-darken-1" rounded :elevation="24">Recordar</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="100" :width="100" color="blue-grey-darken-1" rounded :elevation="24">Comprender</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="100" :width="100" color="blue-grey-darken-1" rounded :elevation="24">Aplicar</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="100" :width="100" color="blue-grey-darken-1" rounded :elevation="24">Analizar</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="100" :width="100" color="blue-grey-darken-1" rounded :elevation="24">Crear</v-sheet>
+            <v-sheet class="knowledgeSquare" :height="100" :width="100" color="blue-grey-darken-1" rounded :elevation="24">Evaluar</v-sheet>
         </section>
     </main>
 </template>
