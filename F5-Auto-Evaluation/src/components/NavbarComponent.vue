a711bacba0f32c566b6816df45e19dac0afa1452
Nafisa <naus7568@gmail.com>
2023-09-25 12:55:20 +0200

    Added navbar

---
+++ F5-Auto-Evaluation/src/components/NavbarComponent.vue
@@ -0,0 +1,42 @@
+<script>
+   
+</script>
+
+<template>
+   <v-toolbar
+   
+    :elevation="8"
+     
+>
+<div>
+  <img class="logo" src="../assets/img/Logo-FF5.png" alt="Logo F5">
+</div>
+<v-spacer></v-spacer>
+
+  <button>Bootcamp</button>
+
+<v-spacer></v-spacer>
+
+<a href="">
+  <img class="userMenu" src="../assets/img/User-menu.png" alt="User Menu">
+</a>
+</v-toolbar>
+  </template>
+
+
+<style scoped>
+.selector{
+    width: 5%;
+}
+.logo{
+height: 62px; 
+margin-left:10px;
+margin-top: 10px;
+}
+.userMenu{
+  width:35px;
+  margin-right:20px;
+  margin-top:10px;
+}
+
+</style>
