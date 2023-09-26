df9404320d6f27c244753ad215467e132d67451c
Angela-coello <angeladarko32@gmail.com>
2023-09-26 14:38:53 +0200

    next commit

--- F5-Auto-Evaluation/src/App.vue
+++ F5-Auto-Evaluation/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import SkillsCard from './components/SkillsCard.vue';
 </script>
 
 <template>
   <RouterView />
+  <SkillsCard/>
 </template>
 
 <style scoped>
---
+++ F5-Auto-Evaluation/src/components/SkillsCard.vue
@@ -0,0 +1,63 @@
+   <script setup>
+
+
+
+</script>
+   
+   
+   
+   
+   
+   
+   
+   <template>
+    <v-card
+      class="mx-auto"
+      max-width="344"
+    >
+      <v-img
+        src="https://img.freepik.com/vector-gratis/ilustracion-concepto-comision-editorial_114360-7751.jpg?size=626&ext=jpg&ga=GA1.2.907985984.1694976088"
+        height="200px"
+        cover
+      ></v-img>
+  
+      <v-card-title>
+        Titulo
+      </v-card-title>
+      <v-divider :thickness="3" color="warning"></v-divider>
+      <br>
+      <v-card-subtitle>
+        Competencia  
+      </v-card-subtitle>
+      <v-card-subtitle>
+        Competencia  
+      </v-card-subtitle>
+      <v-card-subtitle>
+        Competencia  
+      </v-card-subtitle>
+  
+      <v-card-actions>
+        <v-btn class="accessButton"
+          
+          variant="text"
+        >
+          Acceder
+        </v-btn>
+  
+       
+  
+       
+      </v-card-actions>
+  
+      
+    </v-card>
+  </template>
+
+  <style scoped>
+    .accessButton{
+    color: white;
+    background-color: var(--primaryColor);
+    }
+
+</style>
+ 
