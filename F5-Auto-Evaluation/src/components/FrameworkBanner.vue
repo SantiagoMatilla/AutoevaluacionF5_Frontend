977f63a20a582c76ca7e33492cbf92b3831b1827
victorteje <vteje2@gmail.com>
2023-09-26 15:31:12 +0200

    adjusted banner

--- F5-Auto-Evaluation/src/components/FrameworkBanner.vue
+++ F5-Auto-Evaluation/src/components/FrameworkBanner.vue
@@ -3,34 +3,37 @@ import example from "../services/example.json"
 import { ref } from "vue";
 const stack = ref();
 
-fetch (example)
-.then((response) => {
-    stack.value=response.data;
-})
-    
-    
+fetch(example)
+    .then((response) => {
+        stack.value = response.data;
+    })
+
+
 </script>
 
 <template>
     <section class="title">
+        <div class="coderContainer">
+            <v-select class="coderSelection" label="Coder"
+                :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" bg-color="white"
+                density="compact" width="300">
+            </v-select>
+        </div>
         <h1>Front End</h1>
-        <!-- <h1> {{stack.framework}}</h1> -->
-        <v-select class="coderSelection" label="Coder"
-        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" 
-        bg-color="white"
-        density="compact"
-        >
-        </v-select>
+        <v-progress-circular   color="success"
+        bg-color="blue-grey-lighten-3"
+        model-value="20" :size="128" :width="12"></v-progress-circular>
     </section>
 </template>
 
 <style scoped>
 .title {
     background-color: var(--primaryColor);
-    height: 12vh;
+    height: 20vh;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
 }
 
 h1 {
@@ -41,8 +44,10 @@ h1 {
 }
 
 .coderSelection {
-    margin: auto;
-    width: 20%;
     color: white;
+}
+
+.coderContainer {
+    width: 15%;
 }
 </style>
