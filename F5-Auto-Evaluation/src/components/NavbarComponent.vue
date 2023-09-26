d1849d5e14f82f00f91cbdd914e33ac06c859c84
Nafisa <naus7568@gmail.com>
2023-09-26 15:13:38 +0200

    Modified height of navbar and logo

--- F5-Auto-Evaluation/src/components/NavbarComponent.vue
+++ F5-Auto-Evaluation/src/components/NavbarComponent.vue
@@ -3,39 +3,36 @@
 </script>
 
 <template>
-   <v-toolbar
-   
+  <v-toolbar 
     :elevation="8"
-     
->
-<div>
-  <img class="logo" src="../assets/img/Logo-FF5.png" alt="Logo F5">
-</div>
-<v-spacer></v-spacer>
+    class="navbar"
+    height="150"
+    >
+      <div class>
+      <img class="logo" src="../assets/img/Logo-FF5.png" alt="Logo F5">
+      </div>
+      <v-spacer></v-spacer>
 
-  <button>Bootcamp</button>
+      <button>Bootcamp</button>
 
-<v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-<a href="">
-  <img class="userMenu" src="../assets/img/User-menu.png" alt="User Menu">
-</a>
-</v-toolbar>
-  </template>
+    <a href="">
+      <img class="userMenu" src="../assets/img/User-menu.png" alt="User Menu">
+    </a>
+  </v-toolbar>
+</template>
 
 
 <style scoped>
-.selector{
-    width: 5%;
-}
 .logo{
-height: 62px; 
-margin-left:10px;
+height: 100px; 
+margin-left:15px;
 margin-top: 10px;
 }
 .userMenu{
   width:35px;
-  margin-right:20px;
+  margin-right:25px;
   margin-top:10px;
 }
 
