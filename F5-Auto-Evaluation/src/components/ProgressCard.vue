461e77c02ca16126d746407b36b96730aa19be45
Angela-coello <angeladarko32@gmail.com>
2023-10-06 12:10:19 +0200

    initiating test

--- F5-Auto-Evaluation/src/components/ProgressCard.vue
+++ F5-Auto-Evaluation/src/components/ProgressCard.vue
@@ -1,9 +1,9 @@
 <script setup>
-defineProps ({
-   ProgressCard: {
+defineProps({
+  ProgressCard: {
     type: String,
-    cardColor: String, 
-   } 
+    cardColor: String,
+  }
 
 
 })
@@ -13,34 +13,34 @@ defineProps ({
 </script>
 
 <template>
-    <div class="minicard">
-      <!-- <v-card class="d-flex align-center flex-column" width="510"  style="margin-right:30px;" > -->
-        <div   style class="card " >
-          <p  class="text-black">{{ ProgressCard, cardColor }}</p>   
+  <div class="minicard">
 
-        </div>
-        <!-- </v-card> -->
-        <v-progress-circular color="white" bg-color="blue-grey-lighten-3" :size="80" :model-value="25" :width="10" ></v-progress-circular> 
+    <div style class="card ">
+      <p class="text-black">{{ ProgressCard, cardColor }}</p>
+
     </div>
-  </template>
+    <v-progress-circular color="white" bg-color="blue-grey-lighten-3" :size="80" :model-value="25"
+      :width="10"></v-progress-circular>
+  </div>
+</template>
 
-  <style scoped>
-  .minicard {
-    display: flex;
-    width: 70%;
-    margin: 30px
-  }
+<style scoped>
+.minicard {
+  display: flex;
+  width: 70%;
+  margin: 30px
+}
 
-  .card{
-    display: flex;
-    align-content: center;
-    width: 90%;
-    margin-right: 10%;
-    font-size: 30px;
-    text-align: center;
-    background-color: rgb(242, 242, 245);
-    align-items: center;
-    justify-content: center;
-  }
+.card {
+  display: flex;
+  align-content: center;
+  width: 90%;
+  margin-right: 10%;
+  font-size: 30px;
+  text-align: center;
+  background-color: rgb(242, 242, 245);
+  align-items: center;
+  justify-content: center;
+}
 </style>
   
---
+++ F5-Auto-Evaluation/tests/ProgressCard.spec.js
@@ -0,0 +1,7 @@
+import { mount } from '@vue/test-utils';
+import ProgressCard from '@/components/ProgressCard.vue';
+import { describe } from 'vitest';
+
+
+
+
