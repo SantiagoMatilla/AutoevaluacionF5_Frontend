4d498ce3c049a6ac2e198a978b399ae24be9e07d
victorteje <vteje2@gmail.com>
2023-10-06 11:48:26 +0200

    created a service file for each entity

---
+++ F5-Auto-Evaluation/src/services/stackDataService.js
@@ -0,0 +1,25 @@
+import http from "../http-common";
+class stackDataService {
+
+    getAllStacks() {
+        return http.get(`/stack`);
+    }
+
+    getStackById(id) {
+        return http.get(`/stack/${id}`);
+    }
+
+    createStack(data) {
+        return http.post(`/saveStack`, data);
+    }
+
+    updateStack(id, data) {
+        return http.put(`/updateStack/${id}`, data);
+    }
+
+    deleteStack(id) {
+        return http.delete(`/deleteStack/${id}`);
+    }
+}
+
+export default new stackDataService();
