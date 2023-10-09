f310f3b00ee7261852bab9117d9e75bb54c3b08d
victorteje <vteje2@gmail.com>
2023-10-09 12:53:58 +0200

    api call for contents works but brings them all at once

--- F5-Auto-Evaluation/src/components/ContentCard.vue
+++ F5-Auto-Evaluation/src/components/ContentCard.vue
@@ -11,7 +11,6 @@ const props = defineProps({
         <v-card color="#0F1523" width="64%" class="card">
             <div class="d-flex flex-no-wrap justify-space-around align-center">
                 <div class="col-md-4 mb-4">
-                    <!-- v-for="(content, index) in contents" :key="index" -->
                     <v-card-title class="text-h5">
                         {{ content.name }}
                     </v-card-title>
